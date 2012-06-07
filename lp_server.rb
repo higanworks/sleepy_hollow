5854c73d0f23c203363e3bf5bac74fff0bfd6270
sawanoboly <sawanoboriyu@higanworks.com>
2012-06-07 11:55:45 +0900

    fix port option

--- lp_server.rb
+++ lp_server.rb
@@ -1,21 +1,13 @@
 require "bundler"
-require "optparse"
 
 Bundler.setup
 
 require 'sinatra'
 require "eventmachine"
 
-opt = OptionParser.new
+port = ENV["PORT"] ||= "5228"
 
-opt.on('-p =[listening port]', '--port') { |port| $port = port.to_i }
-opt.parse(ARGV)
-
-$port ||= 5228
-
-puts "running on #{$port}"
-
-set :port, $port
+set :port, port
 EM.threadpool_size = 300
 
 get "/" do
